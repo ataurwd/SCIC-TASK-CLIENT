0236e22a4ad8198130ba20ae2c42703044566e8c
ataurwd <ataurrahman24707@gmail.com>
2025-02-20 11:13:02 +0600

    complete to login user sucessfully

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Routes from './routes/Routes';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Routes from "./routes/Routes";
+import LoginContext from "./context/LoginContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Routes/>
-  </StrictMode>,
-)
+    <LoginContext>
+      <Routes />
+    </LoginContext>
+  </StrictMode>
+);
